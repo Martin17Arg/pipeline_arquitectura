d617de415fa9edd82b4f1bcdff8a916668df7974
Martin17Arg <mtosi17@gmail.com>
2023-07-30 21:23:23 -0300

    Reescritura de verificacion_origen

--- verificar_origen.py
+++ verificar_origen.py
@@ -1,14 +1,47 @@
-def verificar_corrida_indicador(
-	indicador: str
-	,periodo: int
-	,conexiones: dict
-	,parametria: dict
-	,query_verificacion: str
-	) -> int:
+def verificar_origen(cliente
+	,parametria_tabla
+	,conexiones
+	,parametria_fechas
+	,query_verificacion
+	,periodo):
 
-	import json
+	""" Verificar que el origen (LKP o FCT) contiene registros y está actualizado """
+
+	# Crear dict vacio para 
+	param = {
+		"tabla": ""
+		,"tipo":""
+		,"conexion": ""
+		,"proyecto": ""
+		,"dataset": ""
+		,"dias_desde_actualizacion": 30
+		,"intervalo_periodos_hacia_atras": 1
+		,"campo_fecha": "Fecha_Key"
+		,"tabla_fechas": ""
+		,"proyecto_fechas": ""
+		,"dataset_fechas": ""
+		}	
+
+	# Cargar conexiones a parametria
+	param.update(parametria_tabla)
+	param.update(conexiones[parametria_tabla["conexion"]])
+	
+	# Cargar parametria de tabla calendario
+	parametria_fechas.update(conexiones[parametria_fechas["conexion"]])
+	param["tabla_fechas"] = parametria_fechas["tabla"]
+	param["proyecto_fechas"] = parametria_fechas["proyecto"]
+	param["dataset_fechas"] = parametria_fechas["dataset"]
+
+	print(param)
+
+	# Ejecutar consulta de verificacion 
 	from google.cloud import bigquery
 
-	# reemplazar parametria en la query template
-	query_
-	# conectar a BQ y correr la consulta
+	query = query_verificacion.format(**param, periodo=periodo)
+	
+	resultados = cliente.query(query).result()
+
+	for row in resultados:
+		condicion = row[0]
+
+	return condicion
